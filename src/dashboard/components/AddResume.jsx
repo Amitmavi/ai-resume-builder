2fbe6d1a6986f54951000d7c1a6d30f598404dac
Amitmavi <amitkumar203536@gmail.com>
2024-10-06 09:35:48 +0530

    fetch global api and save the data in data base

--- src/dashboard/components/AddResume.jsx
+++ src/dashboard/components/AddResume.jsx
@@ -1,4 +1,4 @@
-import { PlusSquare } from 'lucide-react';
+import { Loader2, PlusSquare } from 'lucide-react';
 import React, { useState } from 'react';
 import { 
     Dialog, 
@@ -10,20 +10,46 @@ import {
 import { Button } from "@/components/ui/button"; 
 import { Input } from "@/components/ui/input"; 
 import { v4 as uuidv4 } from 'uuid';   
+import GlobalApi from './../../../service/GlobalApi';
+import { useUser } from '@clerk/clerk-react';
+
+
 
 export default function AddResume() {
   const [openDialog, setOpenDialog] = useState(false);
   const [resumeTitle, setResumeTitle] = useState('');  
+  const {user}=useUser();
+  const [loading,setLoading]=useState(false);
 
   const onCreate=()=>{
+    setLoading(true)
     const uuid=uuidv4();
-    console.log(resumeTitle,uuid);
+    const data={
+      data:{
+        title:resumeTitle,
+        resumeId:uuid,
+        userEmail:user?.primaryEmailAddress?.emailAddress,
+        userName:user?.fullName
+    }
+
+    }
+    GlobalApi.CreateNewResume(data).then(Response=>{
+      console.log(Response);
+      if(Response)
+      {
+        setLoading(false);
+      }
+    },(error)=>{
+      setLoading(false);
+    })
+    
   }
 
   return (
     <div>
       <div 
-        className='p-14 py-24 border items-center flex justify-center bg-secondary rounded-lg h-[280px] hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed'
+        className='p-14 py-24 border items-center flex justify-center bg-secondary rounded-lg h-[280px] 
+        hover:scale-105 transition-all hover:shadow-md cursor-pointer border-dashed'
         onClick={() => setOpenDialog(true)}
       >
         <PlusSquare />
@@ -43,8 +69,12 @@ export default function AddResume() {
             <div className='flex justify-end gap-5'>
               <Button onClick={() => setOpenDialog(false)} variant="ghost">Cancel </Button>
               <Button 
-                disabled={!resumeTitle}  
-                onClick={() =>onCreate()}>  Create </Button>
+                disabled={!resumeTitle||loading}  
+                onClick={() =>onCreate()}>
+                  {
+                    loading?
+                    <Loader2 className='animate-spin'/> :'Create'
+                  }   </Button>
             </div>
           </DialogHeader>
         </DialogContent>
